7b23e678739969776c946133ec3f7568c3922e0a
Hugh Lardner <hughlardner@gmail.com>
2014-06-20 10:13:49 +0100

    view all added

--- database/views.py
+++ database/views.py
@@ -13,33 +13,41 @@ class LoggedInMixin(object):
     @method_decorator(login_required)
     def dispatch(self, *args, **kwargs):
         return super(LoggedInMixin, self).dispatch(*args, **kwargs)
-   
-        
+
+
 def index(request):
     return HttpResponse("Hello, world. You're at the database index.")
-    
+
+class ListCharacterViewAll(LoggedInMixin,ListView):
+
+    model = Character
+    template_name = 'character_list.html'
+
+    def get_queryset(self):
+        return Character.objects.all()
+
 class ListCharacterView(LoggedInMixin,ListView):
 
     model = Character
     template_name = 'character_list.html'
-    
+
     def get_queryset(self):
         return Character.objects.filter(owner=self.request.user)
-    
-    
+
+
 class CreateCharacterView(CreateView):
 
     model = Character
     template_name = 'edit_character.html'
-    
+
     def get_success_url(self):
         return reverse('character-list')
-        
+
     def get_context_data(self, **kwargs):
         context = super(CreateCharacterView, self).get_context_data(**kwargs)
         context['action'] = reverse('character-new')
         return context
-        
+
 class UpdateCharacterView(UpdateView):
 
     model = Character
@@ -47,7 +55,7 @@ class UpdateCharacterView(UpdateView):
 
     def get_success_url(self):
         return reverse('character-list')
-        
+
     def get_context_data(self, **kwargs):
         context = super(UpdateCharacterView, self).get_context_data(**kwargs)
         context['action'] = reverse('character-edit',
@@ -56,9 +64,9 @@ class UpdateCharacterView(UpdateView):
 
 class CharacterView(LoggedInMixin, DetailView):
 
-    model = Character  
+    model = Character
     template_name = 'character.html'
-    
+
     def get_object(self, queryset=None):
         """Returns the object the view is displaying.
 
@@ -78,8 +86,8 @@ class CharacterView(LoggedInMixin, DetailView):
                           {'verbose_name': queryset.model._meta.verbose_name})
 
         return obj
-        
-        
+
+
 def Register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
